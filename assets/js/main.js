505ce41049f4d8a8016ce3ecc9a41a496e8716af
alisher kadraliev <alisherkadraliev@gmail.com>
2023-08-19 09:35:00 +0300

    added pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1318,22 +1318,7 @@ scroll_tl.to('.nasil_races', {
     duration: 1,
     ease: "slow"
 })
-scroll_tl.to(facts, {
-    xPercent: -85 * (facts.length - 1),
-    scrollTrigger: {
-        trigger: ".nasil_races",
-        start: "center center",
-        pin: true,
-        // horizontal: true,
-        // pinSpacing:false,
-        // markers: true,
-        scrub: 1,
-        snap: 1 / (facts.length - 1),
-        // base vertical scrolling on how wide the container is so it feels more natural.
-        // end: () => `+=${smallFactsContainer.offsetWidth}`
-        end: () => `+=4320`
-    }
-});
+
 // FİX THE PROBLEM 
 
 
