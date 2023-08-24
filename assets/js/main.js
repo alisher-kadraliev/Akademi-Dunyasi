a05fa16fe6283eddfa3946bb4c91c30306dc3003
Alisher Kadraliev <alisherkadraliev@gmail.com>
2023-08-24 18:59:01 +0300

    Added bayilik

--- assets/js/main.js
+++ assets/js/main.js
@@ -1213,7 +1213,8 @@ mm.add("(min-width: 600px)", () => {
         trigger: ".gallery",
         start: "top top",
         end: "bottom bottom",
-        pin: ".right"
+        pin: ".right",
+        markers: false,
     });
 
     details.forEach((detail, index) => {
@@ -1319,7 +1320,6 @@ scroll_tl.to('.nasil_races', {
     ease: "slow"
 })
 
-// FİX THE PROBLEM 
 
 
 // Get the window height
@@ -1398,17 +1398,7 @@ tl.to(".preloader", {
     duration: 3,
 });
 
-tl.fromTo(
-    "nav",
-    {
-        opacity: 0,
-    },
-    {
-        opacity: 1,
-        duration: 1,
-    },
-    "-=2"
-);
+
 
 tl.fromTo(
     ".hero-content",
@@ -1423,3 +1413,134 @@ tl.fromTo(
     },
     "-=1.5"
 );
+
+$(function () {
+    var tickerLength = $('.carousel-inner-data ul li').length;
+    var tickerHeight = $('.carousel-inner-data ul li').outerHeight();
+    $('.carousel-inner-data ul li:last-child').prependTo('.carousel-inner-data ul');
+    $('.carousel-inner-data ul').css('marginTop', -tickerHeight);
+
+    function moveTop() {
+        $('.carousel-inner-data ul').animate({
+            top: -tickerHeight
+        }, 600, function () {
+            $('.carousel-inner-data ul li:first-child').appendTo('.carousel-inner-data ul');
+            $('.carousel-inner-data ul').css('top', '');
+        });
+
+    }
+    setInterval(function () {
+        moveTop();
+    }, 3000);
+});
+
+
+$('.slider').each(function () {
+    var $this = $(this);
+    var $group = $this.find('.slide_group');
+    var $slides = $this.find('.slide');
+    var bulletArray = [];
+    var currentIndex = 0;
+    var timeout;
+
+    function move(newIndex) {
+        var animateLeft, slideLeft;
+
+        advance();
+
+        if ($group.is(':animated') || currentIndex === newIndex) {
+            return;
+        }
+
+        bulletArray[currentIndex].removeClass('active');
+        bulletArray[newIndex].addClass('active');
+
+        if (newIndex > currentIndex) {
+            slideLeft = '100%';
+            animateLeft = '-100%';
+        } else {
+            slideLeft = '-100%';
+            animateLeft = '100%';
+        }
+
+        $slides.eq(newIndex).css({
+            display: 'block',
+            left: slideLeft
+        });
+        $group.animate({
+            left: animateLeft
+        }, function () {
+            $slides.eq(currentIndex).css({
+                display: 'none'
+            });
+            $slides.eq(newIndex).css({
+                left: 0
+            });
+            $group.css({
+                left: 0
+            });
+            currentIndex = newIndex;
+        });
+    }
+
+    function advance() {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+            if (currentIndex < ($slides.length - 1)) {
+                move(currentIndex + 1);
+            } else {
+                move(0);
+            }
+        }, 4000);
+    }
+
+    $('.next_btn').on('click', function () {
+        if (currentIndex < ($slides.length - 1)) {
+            move(currentIndex + 1);
+        } else {
+            move(0);
+        }
+    });
+
+    $('.previous_btn').on('click', function () {
+        if (currentIndex !== 0) {
+            move(currentIndex - 1);
+        } else {
+            move(3);
+        }
+    });
+
+    $.each($slides, function (index) {
+        var $button = $('<a class="slide_btn">&bull;</a>');
+
+        if (index === currentIndex) {
+            $button.addClass('active');
+        }
+        $button.on('click', function () {
+            move(index);
+        }).appendTo('.slide_buttons');
+        bulletArray.push($button);
+    });
+
+    advance();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementsByClassName('tab-link')[0].click();
+});
+
+function openTab(evt, tabName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
